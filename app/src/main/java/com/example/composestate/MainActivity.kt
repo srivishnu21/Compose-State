4331d4d24188f36154570783333b432d0a29f229
Sri S40 <ss40@lenovo.com>
2022-07-15 22:09:38 +0530

    State working

---
+++ app/src/main/java/com/example/composestate/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.composestate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+var names: MutableList<String> = mutableListOf<String>("Micheal", "Madan", "Arun", "Foo", "Bar")
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GreetingList()
+    }
+}
+
+@Composable
+fun GreetingList() {
+    val greetingListState = remember { mutableStateListOf<String>("Micheal", "Arun") }
+    for (name in greetingListState) {
+        Greeting(name = name)
+    }
+
+    Button(onClick = { greetingListState.add("vishnu") }) {
+        Text(text = "Add new member")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", style = MaterialTheme.typography.h5)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainScreen()
+}
